272a7518244e2ba1c5c60eb5ce70b96ee6c0fa91
aporwal2801 <aporwal@sapient.com>
2016-12-21 11:49:56 +0530

    Add files via upload

---
+++ Strategy_Pattern/Strategy/AgressiveBehaviour.java
@@ -0,0 +1,11 @@
+package Strategy;
+
+public class AgressiveBehaviour implements IBehaviour{
+	public int moveCommand()
+	{
+		System.out.println("\tAgressive Behaviour: if find another robot attack it");
+		return 1;
+	}
+
+
+}
---
+++ Strategy_Pattern/Strategy/DefensiveBehaviour.java
@@ -0,0 +1,11 @@
+package Strategy;
+
+public class DefensiveBehaviour  implements IBehaviour{
+	public int moveCommand()
+	{
+		System.out.println("\tDefensive Behaviour: if find another robot run from it");
+		return -1;
+	}
+
+
+}
